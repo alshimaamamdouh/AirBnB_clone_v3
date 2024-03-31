f8427cab75e0f9ed0ba7b9077701c684dad3341d
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-03-30 21:01:56 -0700

    task3 fix

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,5 +1,7 @@
 from flask import Blueprint
-from api.v1.views.index import *
+
 
 # Create a variable app_views
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
