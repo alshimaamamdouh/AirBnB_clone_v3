02047b23c7fc2d51067293f1257a917d33ddee8d
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-03-30 20:57:35 -0700

    fix style task3

--- api/v1/app.py
+++ api/v1/app.py
@@ -13,13 +13,16 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 
 # method to handle app
+
+
 @app.teardown_appcontext
 def close_storage(exception):
     storage.close()
 
 # Run  Flask server
+
+
 if __name__ == "__main__":
-    
     # Set host and port
     host = getenv('HBNB_API_HOST', '0.0.0.0')
     port = int(getenv('HBNB_API_PORT', 5000))
