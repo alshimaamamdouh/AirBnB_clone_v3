2a7a509305be946f13b696bf4d72e9eb3558522d
doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-04-01 11:13:33 +0200

    “test_task7”

--- api/v1/views/cities.py
+++ api/v1/views/cities.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """cities"""
 
-from flask import Flask, jsonify, request, abort, make_response
+from flask import Flask, jsonify, request, abort
 from api.v1.views import app_views
 from models import storage
 from models.state import State
@@ -16,12 +16,10 @@ def get_cities(state_id):
     state_obj = storage.get(State, state_id)
     if not state_obj:
         abort(404)
-    cities = storage.all(City)
-    state_cities = []
-    for city in cities.values():
-        if city.state_id == state_id:
-            state_cities.append(city.to_json())
-    return (state_cities)
+    cities = []
+    for city in state_obj.cities:
+        cities.append(city.to_dict())
+    return jsonify(cities)
 
 # handling GET requests to retrieve city obj.
 @app_views.route('/cities/<city_id>', methods=['GET'])
@@ -31,8 +29,6 @@ def get_city_id(city_id):
     if not city:
         abort(404)
     return (jsonify(city.to_dict()))
-
-
 
 
 #  handling DELETE requests to delete a specific city by its ID.
@@ -50,7 +46,7 @@ def delete_city(city_id):
 # handling POST requests to create a new city.
 @app_views.route('/states/<state_id>/cities', strict_slashes=False, methods=['POST'])
 def create_city(state_id):
-    """ create cit in a state """
+    """ create city in a state """
     state_obj = storage.get(State, state_id)
     if not state_obj:
         abort(404)
@@ -60,16 +56,17 @@ def create_city(state_id):
         abort(400, 'Not a JSON')
     if 'name' not in data:
         abort(400, 'Missing name')
-    create_city = City(name=data['name'])
-    storage.new(create_city)
+    
+    new_city = City(name=data['name'], state_id=state_id)
+    storage.new(new_city)
     storage.save()
-    response_data = create_city.to_dict()
-    return make_response(jsonify(response_data), 201)
+    response_data = new_city.to_dict()
+    return jsonify(response_data), 201
 
 
 # handling PUT requests to update an existing state by its ID.
 @app_views.route('cities/<city_id>', strict_slashes=False, methods=['PUT'])
-def updates_cit(city_id):
+def update_city(city_id):
     """ Updates """
     cities = storage.all("City").values()
     city_list = [obj.to_dict() for obj in cities if obj.id == city_id]
