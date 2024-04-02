862aea83fada70b365ccc168d4f22c8da17e4458
alshimaamamdouh <alshimaa.mamdouh.abdelaziz@gmail.com>
2024-04-01 17:58:24 -0700

    task11 with pycodestyle

--- api/v1/views/places_reviews.py
+++ api/v1/views/places_reviews.py
@@ -80,7 +80,8 @@ def update_review(review_id):
         abort(400, "Not a JSON")
     data = request.get_json(force=True, silent=True)
     for key, value in data.items():
-        if key not in ['id', 'user_id', 'place_id', 'created_at', 'updated_at']:
+        if key not in ['id', 'user_id', 'place_id',
+                       'created_at', 'updated_at']: 
             setattr(review, key, value)
     review.save()
     return jsonify(review.to_dict()), 200
